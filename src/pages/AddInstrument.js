f93f68db8b64ff5008a43806e23e265a940f9aac
ttante <tylerdinner@gmail.com>
2016-02-24 01:14:23 -0500

    end of day

--- src/pages/AddInstrument.js
+++ src/pages/AddInstrument.js
@@ -3,11 +3,21 @@ import { Link } from 'react-router';
 import { LoginLink, LogoutLink, NotAuthenticated, Authenticated } from 'react-stormpath';
 import AppBar from 'material-ui/lib/app-bar';
 
+//import Css from './style.js';
+
+var wrapStyle = { 
+  border: '1px solid #666',
+  minHeight: '450px'
+};
+
 //export default AppBarExampleIcon;
 export default class AddInstrument extends React.Component {
+
+  
+
   render() {
     return (
-      <div className="AddInstrument">
+      <div className="AddInstrumentWrap" style={wrapStyle}>
         <AppBar title="New Instrument" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
 
       </div>
